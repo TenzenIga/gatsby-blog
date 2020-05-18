2d203ea36c8824e28210d99df8d016a8dc0dbfa5
TenzenIga <arhan-93@list.ru>
2020-05-18 15:28:41 +0300

    fixed about image

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,10 @@ const IndexPage = () => (
     <h2 className='text-center my-4' >Об авторе</h2>
     <p>Добро пожаловать на мой сайт. Я пока не придумал, что интересного написать о себе.
       Этот блог - попытка систематизировать знания и привести в порядок мысли.</p>
-    <img src={cat} />
+    <div className='text-center'>
+    <img src={cat} className="img-fluid" />
+    </div>
+
   </Layout>
 )
 
