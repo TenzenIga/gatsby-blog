24ce452be1be69cbcf8cc24654db0f9db274d8fd
TenzenIga <arhan-93@list.ru>
2025-09-04 13:29:43 +0300

    changed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,18 +2,18 @@ import React from "react"
 import Layout from "../components/layout"
 import Img from 'gatsby-image';
 import SEO from "../components/seo"
-import cat from '../images/computer-cat.jpg'
+import me from '../images/IMG_6376.jpg'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="About" />
     <div className="about-wrapper">
     <h2 className='text-center my-4' >Об авторе</h2>
-    <p>Всем привет! Меня зовут Архан. Я живу в Санкт-Петербурге и работаю frontend разработчиком больше 4х лет.</p> 
+    <p>Всем привет! Меня зовут Гаджизаде Орхан. Я живу в Санкт-Петербурге и работаю frontend разработчиком больше 4х лет.</p> 
     <p>На этом сайте я делюсь своим опытом, пишу статьи о веб разработке и изучаю что-то новое. Этот блог для меня хобби и попытка систематизировать знания. 
     </p>
     <div className='text-center'>
-    <img src={cat} className="img-fluid" />
+    <img src={me} className="img-fluid" />
     </div>
     </div>
   </Layout>
